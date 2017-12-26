29078eb318e015f7e5527889cde0882bf0f9d3db
Artem Yakushev <Artem Yakushev>
2017-12-26 15:42:05 +0200

    Initial commit

---
+++ app/src/main/java/study/br1221/productivitytimer/MainActivity.java
@@ -0,0 +1,91 @@
+package study.br1221.productivitytimer;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Time;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView remainingTimeTv;
+    Button start, stop, pause;
+
+    TimerService timerService;
+    boolean timerServiceBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        remainingTimeTv = (TextView) findViewById(R.id.remainingTimeTv);
+        start = (Button) findViewById(R.id.startBtn);
+        stop = (Button) findViewById(R.id.stopBtn);
+        pause = (Button) findViewById(R.id.pauseBtn);
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        pause.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver(onEvent , new IntentFilter(TimerService.ACTION_SEND_TIME));
+        Intent intent = new Intent(this, TimerService.class);
+        startService(intent);
+        bindService(intent,serviceConnection, Context.BIND_AUTO_CREATE);
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(onEvent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == start){
+            timerService.startTimer();
+        } else if (view == stop) {
+            timerService.stopTimer();
+        } else if (view == pause) {
+            timerService.pauseTimer();
+        }
+    }
+
+    private BroadcastReceiver onEvent = new BroadcastReceiver() {
+        public void onReceive(Context ctxt, Intent intent) {
+            remainingTimeTv.setText(intent.getStringExtra(TimerService.STRING_TIME));
+        }
+    };
+
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            timerService = ((TimerService.LocalBinder) iBinder).getService();
+            timerServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            timerServiceBound = false;
+        }
+    };
+
+}
---
+++ app/src/main/java/study/br1221/productivitytimer/TimerService.java
@@ -0,0 +1,214 @@
+package study.br1221.productivitytimer;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import android.content.IntentFilter;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+
+import static java.lang.Thread.sleep;
+
+public class TimerService extends Service {
+
+    public static String ACTION_SEND_TIME = "time_send";
+    public static String STRING_TIME = "time_extra_string";
+
+    private boolean isBound = false;
+
+
+    private enum TimerState {STARTED, PAUSED, STOPPED}
+    private volatile TimerState timerState = TimerState.STOPPED;
+
+
+    private Thread timerThread = new Thread(new TimerRunnable());
+
+    int timerNotificationId = 135001;
+
+    TimerActionReceiver actionReceiver = new TimerActionReceiver();
+
+    private final IBinder iBinder = new LocalBinder();
+
+    public static TimerService thisTimerService;
+
+
+    public class LocalBinder extends Binder {
+        TimerService getService() {
+            return TimerService.this;
+        }
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        isBound = true;
+        return iBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        isBound = false;
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        thisTimerService = this;
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        intentFilter.addAction("com.example.app.ACTION_TIMER_START");
+        intentFilter.addAction("com.example.app.ACTION_TIMER_PAUSE");
+        intentFilter.addAction("com.example.app.ACTION_TIMER_STOP");
+        registerReceiver(actionReceiver, intentFilter);
+
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopTimer();
+        unregisterReceiver(actionReceiver);
+    }
+
+    public void startTimer(){
+        thisTimerService = this;
+        synchronized (timerThread) {
+            if (timerState == TimerState.STOPPED) {
+                timerState = TimerState.STARTED;
+                timerThread.start();
+            } else if (timerState == TimerState.PAUSED) {
+                timerState = TimerState.STARTED;
+                timerThread.notifyAll();
+            }
+        }
+    }
+
+
+    public void pauseTimer() {
+        synchronized (timerThread) {
+            if (timerState != TimerState.PAUSED) {
+                timerState = TimerState.PAUSED;
+                timerThread.notify();
+            }
+        }
+    }
+
+    public void stopTimer() {
+
+        if (timerState != TimerState.STOPPED) {
+            synchronized (timerThread) {
+                timerState = TimerState.STOPPED;
+                timerThread.notifyAll();
+            }
+            try {
+                timerThread.join();
+            } catch (InterruptedException e) {
+
+            }
+            timerThread = new Thread(new TimerRunnable());
+        }
+
+    }
+
+
+
+    private void sendTime(String timeStr){
+        Intent intent = new Intent(ACTION_SEND_TIME);
+        intent.putExtra(STRING_TIME, timeStr);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    private void outputNotification(String time){
+        NotificationManager NotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent intentMainActivity = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntentMainActivity = PendingIntent.getActivity(this, 1, intentMainActivity, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent startIntent = new Intent("com.example.app.ACTION_TIMER_START");
+        PendingIntent startPendingIntent = PendingIntent.getBroadcast(this, 100, startIntent, 0);
+
+        Intent pauseIntent = new Intent("com.example.app.ACTION_TIMER_PAUSE");
+        PendingIntent pausePendingIntent = PendingIntent.getBroadcast(this, 100, pauseIntent, 0);
+
+
+        Intent stopIntent = new Intent("com.example.app.ACTION_TIMER_STOP");
+        PendingIntent stopPendingIntent = PendingIntent.getBroadcast(this, 100, stopIntent, 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle("My notification")
+                        .setContentText(time)
+                        .setContentIntent(pendingIntentMainActivity)
+                        .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher_round, "Start", startPendingIntent))
+                        .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher_round, "Pause", pausePendingIntent))
+                        .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher_round, "Stop", stopPendingIntent));
+        startForeground(timerNotificationId, mBuilder.build());
+
+    }
+
+
+
+    private class TimerRunnable implements Runnable {
+        @Override
+        public void run() {
+            synchronized (timerThread) {
+                while (timerState == TimerState.STARTED) {
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                    String time = sdf.format(new Date());
+                    sendTime(time);
+                    outputNotification(time);
+                    try {
+                        timerThread.wait(1000);
+                    } catch (InterruptedException e) {
+
+                    }
+                    while (timerState == TimerState.PAUSED) {
+                        try {
+                            timerThread.wait();
+                        } catch (InterruptedException e) {
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static class TimerActionReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if(action.equalsIgnoreCase("com.example.app.ACTION_TIMER_START")){
+                if(thisTimerService != null) thisTimerService.startTimer();
+            } else if(action.equalsIgnoreCase("com.example.app.ACTION_TIMER_PAUSE")){
+                if(thisTimerService != null) thisTimerService.pauseTimer();
+            } else if(action.equalsIgnoreCase("com.example.app.ACTION_TIMER_STOP")){
+                if(thisTimerService != null) thisTimerService.stopTimer();
+            }
+        }
+    }
+}
