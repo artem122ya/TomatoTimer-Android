b3b6acc6686755bd9ef1138ca8ffdeeca9aff7d8
artem122ya <artemyakushev2014@gmail.com>
2018-02-12 01:06:19 +0200

    Made settings activity retain scroll position on configuration change

--- app/src/main/java/artem122ya/tomatotimer/settings/SettingsActivity.java
+++ app/src/main/java/artem122ya/tomatotimer/settings/SettingsActivity.java
@@ -1,15 +1,9 @@
 package artem122ya.tomatotimer.settings;
 
 
-import android.content.Intent;
+import android.app.Fragment;
 import android.content.SharedPreferences;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.MultiSelectListPreference;
-import android.preference.Preference;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -30,7 +24,11 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new TimerPreferenceFragment()).commit();
+        Fragment existingFragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (existingFragment == null) {
+            getFragmentManager().beginTransaction().replace(android.R.id.content,
+                    new TimerPreferenceFragment()).commit();
+        }
     }
 
     @Override
@@ -44,8 +42,7 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.dark_mode_preference_key))) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            finish();
+            recreate();
         }
     }
 
@@ -72,74 +69,6 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public static class TimerPreferenceFragment extends PreferenceFragment implements
-            SharedPreferences.OnSharedPreferenceChangeListener {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-            PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
-            initSummary(getPreferenceScreen());
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-            // Set up a listener whenever a key changes
-            getPreferenceScreen().getSharedPreferences()
-                    .registerOnSharedPreferenceChangeListener(this);
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-            // Unregister the listener whenever a key changes
-            getPreferenceScreen().getSharedPreferences()
-                    .unregisterOnSharedPreferenceChangeListener(this);
-        }
-
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-                                              String key) {
-            updatePrefSummary(findPreference(key));
-        }
-
-        private void initSummary(Preference p) {
-            if (p instanceof PreferenceGroup) {
-                PreferenceGroup pGrp = (PreferenceGroup) p;
-                for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
-                    initSummary(pGrp.getPreference(i));
-                }
-            } else {
-                updatePrefSummary(p);
-            }
-        }
-
-        private void updatePrefSummary(Preference p) {
-            if (p instanceof ListPreference) {
-                ListPreference listPref = (ListPreference) p;
-                modifySummary(p, String.valueOf(listPref.getEntry()));
-            }
-            if (p instanceof EditTextPreference) {
-                EditTextPreference editTextPref = (EditTextPreference) p;
-                modifySummary(p, editTextPref.getText());
-            }
-            if (p instanceof MultiSelectListPreference) {
-                EditTextPreference editTextPref = (EditTextPreference) p;
-                modifySummary(p, editTextPref.getText());
-            }
-            if (p instanceof NumberPickerDialogPreference) {
-                NumberPickerDialogPreference minutePickerPref = (NumberPickerDialogPreference) p;
-                modifySummary(p, String.valueOf(minutePickerPref.getValue()));
-            }
-        }
-
-        private void modifySummary(Preference preference, String value){
-            String summary = String.valueOf(preference.getSummary());
-            summary = summary.replaceFirst("\\d{1,4}", value);
-            preference.setSummary(summary);
         }
     }
 }
---
+++ app/src/main/java/artem122ya/tomatotimer/settings/TimerPreferenceFragment.java
@@ -0,0 +1,85 @@
+package artem122ya.tomatotimer.settings;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
+
+import artem122ya.tomatotimer.R;
+
+
+public class TimerPreferenceFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
+        initSummary(getPreferenceScreen());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        updatePrefSummary(findPreference(key));
+    }
+
+    private void initSummary(Preference p) {
+        if (p instanceof PreferenceGroup) {
+            PreferenceGroup pGrp = (PreferenceGroup) p;
+            for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                initSummary(pGrp.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
+    }
+
+    private void updatePrefSummary(Preference p) {
+        if (p instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) p;
+            modifySummary(p, String.valueOf(listPref.getEntry()));
+        }
+        if (p instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            modifySummary(p, editTextPref.getText());
+        }
+        if (p instanceof MultiSelectListPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            modifySummary(p, editTextPref.getText());
+        }
+        if (p instanceof NumberPickerDialogPreference) {
+            NumberPickerDialogPreference minutePickerPref = (NumberPickerDialogPreference) p;
+            modifySummary(p, String.valueOf(minutePickerPref.getValue()));
+        }
+    }
+
+    private void modifySummary(Preference preference, String value){
+        String summary = String.valueOf(preference.getSummary());
+        summary = summary.replaceFirst("\\d{1,4}", value);
+        preference.setSummary(summary);
+    }
+}
